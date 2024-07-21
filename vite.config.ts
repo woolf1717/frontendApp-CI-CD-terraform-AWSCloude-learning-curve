f0d857d6fb18be059f85bdee991ee2af96d7f211
woolf1717 <Barstpiczynski@gmail.com>
2024-07-21 11:32:55 +0200

    Config changes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import packageJson from './package.json';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    sourcemap: true,
+  },
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    ViteImageOptimizer({
+      png: {
+        quality: 50,
+      },
+    }),
+  ],
 });
