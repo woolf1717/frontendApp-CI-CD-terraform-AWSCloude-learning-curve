1aee42009026fcb60e092c2851162e6079f211fd
woolf1717 <Barstpiczynski@gmail.com>
2024-07-21 12:27:25 +0200

    Deploy test.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
+import React from 'react';
 
 const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service</h1>
+        <h1 className="text-xl font-bold text-center">🚀 Hello Rick and Morty - Fan Service</h1>
       </div>
       <Outlet />
     </div>
