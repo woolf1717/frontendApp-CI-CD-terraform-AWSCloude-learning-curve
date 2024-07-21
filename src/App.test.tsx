a10de27bec25c24f0133afcabbc156d0f7096dc1
woolf1717 <Barstpiczynski@gmail.com>
2024-07-21 13:20:32 +0200

    Added tests while deploying on Neltify.

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+
+import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+describe('App component', () => {
+  it('renders the App component with the correct heading', () => {
+    const html = renderToString(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(html).toContain('🚀 Hello Rick and Morty - Fan Service');
+  });
+});
