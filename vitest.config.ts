c406cf73d55bfe24ba7bffdb9f6ec0500f30b466
woolf1717 <Barstpiczynski@gmail.com>
2024-07-27 21:33:03 +0200

    Testing checks.

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { configDefaults, defineConfig } from 'vitest/config';
-
-const PLAYWRIGHT_TESTS = ['**/*.spec.ts'];
-
-export default defineConfig({
-  test: {
-    exclude: [...configDefaults.exclude, ...PLAYWRIGHT_TESTS],
-  },
-});
