935c0ba1997ab8c1dbb560147bac96a22cf55e77
woolf1717 <Barstpiczynski@gmail.com>
2024-07-27 22:52:59 +0200

    Pull

--- tests/e2e/tests/example.spec.ts
+++ tests/e2e/tests/example.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test';
 test('example test', async ({ page }) => {
   console.log('E2E_BASE_URL', process.env.E2E_BASE_URL);
   await page.goto('/'); // Playwright will use the baseURL set in the config
-  expect(await page.title()).toBe('Hello!');
+  expect(await page.title()).toBe('Login – Vercel');
 });
